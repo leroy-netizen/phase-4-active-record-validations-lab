6c176fa0eae781351d8ea2a3102582c1da9f38b9
= <=>
2022-10-03 10:35:53 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: %w!Fiction Non-Fiction!}
+	validate :is_clickbait?
+
+	def is_clickbait?
+		valids = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+		if valids.none?{|par| par.match title}
+			errors.add(:title, "not clickbait")
+		end
+	end
 end
